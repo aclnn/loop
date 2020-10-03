02f4c1cf057a192d0f8cc67efc423e77e32b84e6
Jacques <jacques.clery--salenave@u-psud.fr>
2020-10-03 15:12:42 +0200

    obstacle spawn

--- Assets/Scripts/PathObstacleGenerator.cs
+++ Assets/Scripts/PathObstacleGenerator.cs
@@ -2,59 +2,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using DefaultNamespace;
 using PathCreation;
 using UnityEngine;
 
 [RequireComponent(typeof(PathCreator))]
 public class PathObstacleGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject obstaclePoint;
     public PathCreator pathCreator;
     //Si c'est trop bas le drawgizmos va vouloir draw trop de sphere et ça crash/répond dans longtemps (genre 0.0003)
-    [SerializeField] [Range(0.001f, 0.1f)] private float obstacleTileLength = 0.02f;
+    [SerializeField] private float obstacleTileLength = 1f;
+    [SerializeField] private Vector3 obstacleOffset;
 
-    private List<Vector3> obstaclePoints = new List<Vector3>();
+    private List<GameObject> obstaclePoints = new List<GameObject>();
     
     //Dans l'inspector -> les 3 petits points ça ajoute un bouton pour lancer la fonction
     [ContextMenu("Start")]
     void Start()
     {
         pathCreator = GetComponent<PathCreator>();
-        
-        obstaclePoints.Clear();
-        
-        for (float i = 0; i < 1; i += obstacleTileLength)
-        {
-            //pathCreator.path.GetPointAtTime(i) récupère les points du chemin en fonction du temps, de 0 à 1
-            obstaclePoints.Add(pathCreator.path.GetPointAtTime(i));
-        }
+
+        InitObstaclePos();
     }
     void Update()
     {
-        pathCreator.path.GetPointAtTime(obstacleTileLength);
     }
 
+    private void InitObstaclePos()
+    {
+        obstaclePoints.Clear();
+        
+        for (float i = 0; i < pathCreator.path.length; i += obstacleTileLength)
+        {
+            //pathCreator.path.GetPointAtTime(i) récupère les points du chemin en fonction du temps, de 0 à 1
+            var instantiatedObj = Instantiate(obstaclePoint);
+            instantiatedObj.transform.position = pathCreator.path.GetPointAtDistance(i) + obstacleOffset;
+            instantiatedObj.transform.localRotation = new Quaternion(
+                pathCreator.path.GetRotationAtDistance(i).x, 
+                pathCreator.path.GetRotationAtDistance(i).y,
+                pathCreator.path.GetRotationAtDistance(i).z,
+                pathCreator.path.GetRotationAtDistance(i).w);
+            obstaclePoints.Add(instantiatedObj);
+        }
+    }
+    
+    [ContextMenu("SpawnObstacles")]
     private void SpawnObstacles()
     {
-        
+        foreach (var point in obstaclePoints)
+        {
+            point.GetComponent<ObstacleBehaviour>().SpawnObstacle(ObstacleEnum.LEFT);
+            point.GetComponent<ObstacleBehaviour>().SpawnObstacle(ObstacleEnum.MIDDLE);
+            point.GetComponent<ObstacleBehaviour>().SpawnObstacle(ObstacleEnum.RIGHT);
+        }
     }
-    
-    
-    #if UNITY_EDITOR
+
+    [ContextMenu("DestroyObstacles")]
+    private void DestroyObstacles()
+    {
+        foreach (var point in obstaclePoints)
+        {
+            point.GetComponent<ObstacleBehaviour>().DestroyObstacles();
+        }
+    }
+
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        
-        obstaclePoints.Clear();
-
-        for (float i = 0; i < 1; i += obstacleTileLength)
-        {
-            obstaclePoints.Add(pathCreator.path.GetPointAtTime(i));
-        }
-        
         Gizmos.color = Color.red;
-        foreach (Vector3 point in obstaclePoints)
+        
+        for (int i = 0; i < obstaclePoints.Count; i++)
         {
-            Gizmos.DrawWireSphere(point, 1f);
+            Gizmos.DrawWireSphere(obstaclePoints[i].transform.position, 1f);
         }
     }
-    #endif
+#endif
+    
 }
