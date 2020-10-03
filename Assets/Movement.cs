db74a6d58256586476ee6d4313e5261c6e51df3f
Alexandre Calonne <alexandrecalonne59@gmail.com>
2020-10-03 22:46:37 +0200

    Ship rotation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using PathCreation.Examples;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 
 public class Movement : MonoBehaviour
 {
@@ -28,6 +29,19 @@ public class Movement : MonoBehaviour
             
             position.x = Mathf.Clamp(position.x, -roadMeshCreator.roadWidth + clampOffset, roadMeshCreator.roadWidth - clampOffset);
             transform.localPosition = position;
+
+            TiltShip();
         }
+    }
+
+    void TiltShip()
+    {
+        float shipXAngle = transform.localEulerAngles.x;
+        shipXAngle = shipXAngle > 180 ? shipXAngle - 360 : shipXAngle;
+        
+        float tiltAngle = Mathf.Clamp(Input.GetAxis("Mouse X") * 2, -1F, 1F) * -30;
+        float smoothTiltAngle = Mathf.Lerp(shipXAngle, tiltAngle, .1F);
+        
+        transform.localEulerAngles = new Vector3(smoothTiltAngle, -90, 0);
     }
 }
