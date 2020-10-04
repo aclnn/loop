6cf31d96239599aa15a673545baabea1f6014302
Alexandre Calonne <alexandrecalonne59@gmail.com>
2020-10-04 23:15:31 +0200

    Side trigger particles

--- Assets/SideTrigger.cs
+++ Assets/SideTrigger.cs
@@ -6,10 +6,23 @@ using UnityEngine;
 public class SideTrigger : MonoBehaviour
 {
     [SerializeField] private DrillBehaviour drillBehaviour;
+    [SerializeField] private GameObject sideTriggerParticles;
     
     private void OnTriggerExit(Collider other)
     {
         drillBehaviour.IncreaseDrillCharge(1);
         transform.parent.GetComponent<PlayerBehaviour>().SideTrigger();
+        
+        SpawnSideTriggerParticles(other.transform.position);
+    }
+
+    private void SpawnSideTriggerParticles(Vector3 otherPosition)
+    {
+        Vector3 particlesPosition = new Vector3(
+            (transform.position.x + otherPosition.x) / 2F,
+            (transform.position.y + otherPosition.y) / 2F,
+            (transform.position.z + otherPosition.z) / 2F
+        );
+        Instantiate(sideTriggerParticles, particlesPosition, Quaternion.identity, transform.parent);
     }
 }
