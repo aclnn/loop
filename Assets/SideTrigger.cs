91fc3dba41f0d086c608326bc8935219db79940f
Jacques <jacques.clery--salenave@u-psud.fr>
2020-10-04 15:17:27 +0200

    scene saved & ontriggerleave

--- Assets/SideTrigger.cs
+++ Assets/SideTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class SideTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerLeave(Collider other)
     {
         transform.parent.GetComponent<PlayerBehaviour>().SideTrigger();
     }
