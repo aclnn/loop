d07b993517e0c49b1336133d420f43b15f019ab0
Jacques <jacques.clery--salenave@u-psud.fr>
2020-10-04 23:58:12 +0200

    cursor changes & tried build stuff

--- Assets/CursorScript.cs
+++ Assets/CursorScript.cs
@@ -7,9 +7,12 @@ public class CursorScript : MonoBehaviour
     public void DisableCursor()
     {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
     }
+
     public void EnableCursor()
     {
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
