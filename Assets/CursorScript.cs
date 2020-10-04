f37d6a47e9551c88c3352791586325ac0dc2b65c
Jacques <jacques.clery--salenave@u-psud.fr>
2020-10-04 17:27:52 +0200

    hide cursor in game

---
+++ Assets/CursorScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public void DisableCursor()
+    {
+        Cursor.visible = false;
+    }
+    public void EnableCursor()
+    {
+        Cursor.visible = true;
+    }
+}
